ca84ead67b1b32d68d80ea9ca31c996602918b8a
Marty Neal <marty.neal@interprose.com>
2021-12-06 20:08:30 -0800

    Days 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,30 +3,23 @@ import java.lang.Integer.parseInt
 fun main() {
     fun part1(input: List<Int>, days: Int = 80): Int {
         if (days == 0) return input.size
-        return part1(input.map { if(it == 0) 6 else it - 1 } + List(input.count { it == 0 }) { 8 } , days - 1)
+        return part1(input.map { if (it == 0) 6 else it - 1 } + List(input.count { it == 0 }) { 8 }, days - 1)
     }
 
     fun part2(input: List<Int>, days: Int = 256): Long {
         fun helper(input: Map<Int, Long>, days: Int): Long =
             if (days == 0)
                 input.values.sum()
-            else
-                helper(input.let { mapOf(
-                    0 to it.getOrDefault(1, 0),
-                    1 to it.getOrDefault(2, 0),
-                    2 to it.getOrDefault(3, 0),
-                    3 to it.getOrDefault(4, 0),
-                    4 to it.getOrDefault(5, 0),
-                    5 to it.getOrDefault(6, 0),
-                    6 to it.getOrDefault(7, 0) + it.getOrDefault(0, 0),
-                    7 to it.getOrDefault(8, 0),
-                    8 to it.getOrDefault(0, 0),
-                ) }, days - 1)
+            else {
+                helper((0..8).associateWith { input.getValue((it + 1) % 9) + if (it == 6) input.getValue(0) else 0 }, days - 1)
+            }
 
         return helper(input
             .groupingBy { it }
             .eachCount() // perhaps my favorite stdlib function
-            .mapValues { it.value.toLong() }, days)
+            .mapValues { it.value.toLong() }
+            .withDefault { 0L }, days
+        )
     }
 
     // test if implementation meets criteria from the description, like:
